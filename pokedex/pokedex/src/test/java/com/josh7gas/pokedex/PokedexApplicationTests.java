4ce29774f3088b26e7d45cc1378935f3064257ed
Josh7GAS <joshuaandre@gmail.com>
2020-07-16 18:05:35 -0300

    Criando o java application e subindo o server

---
+++ pokedex/pokedex/src/test/java/com/josh7gas/pokedex/PokedexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josh7gas.pokedex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokedexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
